c3d09de08172ed98619aad7514bf08b3d6547967
William Harris <willmharris0@gmail.com>
2020-01-19 21:38:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,7 +10,9 @@ def introduction_with_language_optional (name, language = "Ruby")
 puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-
+#def introduction_with_language_optional(name, language="Ruby")
+#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+#end
 
 
 
